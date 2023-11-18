a1bffca64224789d2472eade787434fbdc890c24
Mudhaffar <Mudhaffar.Ngwali@gmail.com>
2023-11-18 16:44:17 -0500

    Added more animations and user feedback

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ function App() {
 	const [quote, setQuote] = 
 	useState({id:0, advice:"Click on the button below to generate an inspirational quote!"});
 
+	const [state, setState] = useState("Idle");
+
 	async function generateQuote() {
+		setState("Loading");
+		
 		const requestURL = "https://api.adviceslip.com/advice";
 		const request = new Request(requestURL);
 	
@@ -15,7 +19,9 @@ function App() {
 		const ID = quote.slip.id;
 		const ADVICE = quote.slip.advice;
 
+		setState("Loaded");
 		setQuote({id:ID, advice:ADVICE});
+		setTimeout(() => { setState("Idle"); }, 1300);
 	}
 
 
@@ -23,19 +29,25 @@ function App() {
 		<div className="min-h-screen w-full
 						flex items-center justify-center 
 						font-primary bg-neutral-three text-center pb-8">
-			<main className='max-w-[30rem] px-4 sm:px-8 pt-12 pb-16 mx-3 my-4 bg-neutral-two 
-							rounded-xl relative space-y-6 shadow-xl shadow-neutral-three'>
+			<main className='max-w-[32rem] px-4 sm:px-8 pt-12 pb-16 mx-6 my-4 bg-neutral-two 
+							rounded-xl relative space-y-6 shadow-lg shadow-neutral-three'>
 				<h1 className="text-primary-two tracking-widest uppercase">Advice #{quote.id}</h1>
-				<p className="text-primary-one text-base">
-					{quote.advice}
+				<p className={"text-primary-one text-base " + (state === "Loaded" ? "fadeIn" : "")}>
+				“{quote.advice}”
 				</p>
 				<img className="divider-mobile block mx-auto xs:divider-desktop" alt="Divider" />
-				<button className="w-16 aspect-square rounded-full bg-primary-two absolute
-								bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2
-								flex items-center justify-center
-								hover:button-shadow active:shadow-none transition-all"
-						onClick={generateQuote}>
-					<img src={DiceImg} alt="Dice_img" />
+				<button className={"w-16 aspect-square rounded-full absolute \
+								bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 \
+								flex items-center justify-center \
+								transition-all duration-300 "
+								+ (state === "Idle" ? 
+								"bg-primary-two hover:button-shadow active:shadow-none" : 
+								"bg-neutral-one"
+								)}
+						disabled={state !== "Idle"}
+						onClick={generateQuote}
+						>
+					<img src={DiceImg} alt="Dice_img" className={state !== "Idle" ? "spin" : ""} />
 				</button>
 			</main>
 		</div>
